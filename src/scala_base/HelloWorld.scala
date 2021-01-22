5a5cdc85468d47d0c6052aadfd068e3495f153a8
李娜 <lina.2017@bytedance.com>
2021-01-22 13:53:37 +0800

    init

---
+++ src/scala_base/HelloWorld.scala
@@ -0,0 +1,11 @@
+package scala_base
+
+/**
+ * @author lina.2017
+ * @date 2021/1/22 12:05 下午
+ */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+  }
+}
